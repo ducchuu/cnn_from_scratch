6007911af2ef4fe440644be3af14067470548d57
ducchuu <165240940+ducchuu@users.noreply.github.com>
2025-03-04 17:35:40 +0100

    [NN and CNN]

---
+++ src/00_research/Mikolaj/NN/layer.py
@@ -0,0 +1,9 @@
+class Layer:
+    def __init__(self):
+        self.input = None
+        self.output = None
+
+    def forward_pass(self, input):
+        pass
+    def backward_propagation(self, output_gradient, learning_rate):
+        pass
